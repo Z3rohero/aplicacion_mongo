c4bafa7534d2b85c45aaf1658abca3433a605d96
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-08-09 16:33:40 +0000

    punto de partida

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import indexRoutes from './routes/routes.js'
 const app = express();
 
 app.listen(3000);
+app.use(indexRoutes);
 
 //app.send("hola como fue")
 console.log('serve on port', 3000);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,9 @@ import {Router} from 'express';
 const router = Router()
 
 
-
 router.get("/",(req,res) => {
   res.send("hello world")
 });
 
-export default route; 
+export default router; 
 
