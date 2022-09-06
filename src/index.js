685be3eb027607425f83544e37655cf024952ee1
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-09-06 20:05:09 +0000

    masterrr

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import app  from "./app";
 import  {connecToDB}   from './databases.js'
 
 connecToDB();
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('serve on port', 3000);
+
+
+
 
